cee8dd445ab946414e68fc6c2d4da2c1f8f4fb64
MicaelLoio <131375089+MicaelLoio@users.noreply.github.com>
2023-11-20 15:38:37 +0000

    1.TIme
    inserting a script on each Adams.

---
+++ Assets/Script/SecondsUpdate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondsUpdate : MonoBehaviour
+{
+
+    float timeStartoffset = 0;
+    bool gotStartTime = false;
+
+    void Update()
+    {
+        if(!gotStartTime)
+        {
+            timeStartoffset = Time.realtimeSinceStartup;
+            gotStartTime = true;
+        }
+        this.transform.position = new Vector3(this.transform.position.x,
+                                                this.transform.position.y,
+                                                    Time.realtimeSinceStartup - timeStartoffset);
+    }
+}
