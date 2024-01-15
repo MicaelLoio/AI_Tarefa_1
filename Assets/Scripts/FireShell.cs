b7d4f6d5fb46b914c3cb8f822b4774d998c97afd
MicaelLoio <131375089+MicaelLoio@users.noreply.github.com>
2024-01-15 16:28:51 +0000

    Calculating Trajectories Part 2
    Não foi possivel completar pois o script encontra.se com alguns erros de escrita apesar de ter sido confirmado inumeras vezes com o script que se encontra no video.

--- Assets/Scripts/FireShell.cs
+++ Assets/Scripts/FireShell.cs
@@ -7,51 +7,59 @@ public class FireShell : MonoBehaviour {
     public GameObject bullet;
     public GameObject turret;
     public GameObject enemy;
+    float speed = 15;
+    float rotSpeed =2;
 
     void CreateBullet() {
 
         Instantiate(bullet, turret.transform.position, turret.transform.rotation);
     }
 
-    void Update() {
+          void RotateTurret()
+          {
+                float? angle = CalculateAngle (true);
+                if (angle !=null)
+                {
+                    turretBase.localEulerAngles = new Vector3(360f - (float)angle, 0f, 0f);
+                }
+          }
 
+    float? CalculateAngle(bool low)
+    {
+        Vector3 targetDir = enemy.transform.position - this.transform.position;
+        float y =  tagetDir.y;
+        targetDir.y = 0f;
+        float x = targetDir.magnitude;
+        float gravity = 9.8f;
+        float sSq = speed * speed;
+        float  underTheSqr = (sSq * sSq) - gravity * (gravity * x * x + 2 * y * sSq);
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (underTheSqr >= 0f)
+        {
+            float root = Mathf.Sqrt(underTheSqrRoot);
+            float highAngle = sSqr + root;
+            float lowAngle = sSqr - root;
+            
 
-            Vector3 aimAt = CalculateTrajectory();
-            if (aimAt != Vector3.zero) {
-
-                this.transform.forward = CalculateTrajectory();
-                CreateBullet();
-            }
+            if (low)
+                return (Mathf.Atan2(lowAngle, gravity * x) * Mathf.Rad2Deg);
+            else
+                return (Mathf.Atan2(highAngle, gravity * x) * Mathf.Rad2Deg);
         }
+        else    
+            return null;
     }
 
-    Vector3 CalculateTrajectory() {
+    void Update() 
+    {
 
-        Vector3 p = enemy.transform.position - this.transform.position;
-        Vector3 v = enemy.transform.forward * enemy.GetComponent<Drive>().speed;
-        float s = bullet.GetComponent<MoveShell>().speed;
-
-        float a = Vector3.Dot(v, v) - s * s;
-        float b = Vector3.Dot(p, v);
-        float c = Vector3.Dot(p, p);
-        float d = b * b - a * c;
-
-        if (d < 0.1f) return Vector3.zero;
-
-        float sqrt = Mathf.Sqrt(d);
-        float t1 = (-b - sqrt) / c;
-        float t2 = (-b + sqrt) / c;
-
-        float t = 0.0f;
-        if (t1 < 0.0f && t2 < 0.0f) return Vector3.zero;
-        else if (t1 < 0.0f) t = t2;
-        else if (t2 < 0.0f) t = t1;
-        else {
-
-            t = Mathf.Max(new float[] { t1, t2 });
-        }
-        return t * p + v;
+     Vector3 direction = (enemy.transform.position - this.transform.position).normalized;
+     Quaternion lookRotation = Quaternion.lookRotation(new Vector3(direction.x, 0 , direction.z));                                                      
+     this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookRotation, Time.deltaTime * rotSpeed);
+     RotateTurret();
+     if (Input.GetKeyDown(KeyCode.Space))
+     {
+         CreateBullet();
+     }
     }
 }
